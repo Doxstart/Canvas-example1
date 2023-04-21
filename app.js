0d7d48d5c4d04f27c5fc77c9c344e44de3215244
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-04-21 13:00:28 +0200

    lez. 21/04/2023

--- app.js
+++ app.js
@@ -2,36 +2,78 @@ const myCanvas = document.getElementById('my-canvas');
 
 const ctx = myCanvas.getContext('2d');
 
-const point = {x: 300, y: 300};
+let newGeneration = Generation.generateRandom(80, myCanvas.width, myCanvas.height);
 
-setInterval(() => {
-    const randomX = (Math.random() * 4)-2; 
-    const randomY = (Math.random() * 4)-2; 
-    point.x += randomX;
-    point.y += randomY;
+// for (let i = 0; i < 50000; i++) {
+//     const newPoint = Point.generateRandom(myCanvas.width, myCanvas.height);
+//     newGeneration.addPoint(newPoint);
+// }
 
-    ctx.fillStyle = 'red';
+console.log(newGeneration);
 
-    ctx.fillRect(point.x, point.y, 10, 10);
-}, 0.1);
+// setInterval(() =>{
 
-for (let i = 0; i < 100; i++) {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
+//     for (let i = 0; i < pointsArray.length; i++) {
+//         const point = pointsArray[i];
+//         point.draw(ctx);
+//         point.changePosition();
+//     }
+// }, 1);
 
-    const point = {x: 300, y: 300};
+function step(){
+    // for (let i = 0; i < newGeneration.pointsArray.length; i++) {
+    //     const point = newGeneration.pointsArray[i];
+    //     point.draw(ctx);
+    //     point.changePosition();
+    // }
+    newGeneration.draw(ctx);
 
-    point.color = `rgba(${red},${green},${blue})`;
-    
-    setInterval(() => {
-        const randomX = (Math.random() * 4)-2; 
-        const randomY = (Math.random() * 4)-2; 
-        point.x += randomX;
-        point.y += randomY;
-    
-        ctx.fillStyle = point.color;
-    
-        ctx.fillRect(point.x, point.y, 10, 10);
-    }, 0.1);
+    if (newGeneration.isDead()) {
+        ctx.fillStyle = 'rgba(0,0,0,0.5)'
+        ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
+        newGeneration = Generation.generateRandom(80, myCanvas.width, myCanvas.height);
+    }
+
+    window.requestAnimationFrame(step);
 }
+
+window.requestAnimationFrame(step);
+
+//const point = {x: 300, y: 300};
+
+// setInterval(() => {
+//     const randomX = (Math.random() * 4)-2; 
+//     const randomY = (Math.random() * 4)-2; 
+//     point.x += randomX;
+//     point.y += randomY;
+
+//     ctx.fillStyle = 'red';
+
+//     ctx.fillRect(point.x, point.y, 1.5, 1.5);
+// }, 0.1);
+
+// for (let i = 0; i < 100; i++) {
+//     // const red = Math.floor(Math.random() * 256);
+//     // const green = Math.floor(Math.random() * 256);
+//     // const blue = Math.floor(Math.random() * 256);
+
+//     // //const randomColor = {x: 300, y: 300};
+//     // const point = new Point(300, 300, randomColor);
+
+//     // const randomColor = `rgb(${red},${green},${blue})`;
+
+//     const point = Point.generateRandom(myCanvas.width, myCanvas.height);
+    
+//     setInterval(() => {
+//         point.draw(ctx);
+//         point.changePosition();
+//         // const randomX = (Math.random() * 4)-2; 
+//         // const randomY = (Math.random() * 4)-2; 
+//         // point.x += randomX;
+//         // point.y += randomY;
+    
+//         // ctx.fillStyle = point.color;
+    
+//         // ctx.fillRect(point.x, point.y, 1.5, 1.5);
+//     }, 0.1);
+// }
